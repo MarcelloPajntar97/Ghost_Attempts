5e1ce38167d5beabdc53faddce75e32e02f98803
Guy97 <marcello.pajntar@ied.edu>
2019-11-16 16:57:01 +0100

    momentaneamente risolto problema connessione, da migliorare

--- app/Http/Controllers/Connection.php
+++ app/Http/Controllers/Connection.php
@@ -11,14 +11,20 @@ class Connection extends Controller
     public function sshconn() {
     	$command = base_path().'/vendor/bin/envoy run deploy';
     	$directory = base_path();
-    	$process = new Process($command);
+        //$open = new Process('cd ..');
+        //$content = ['cd ..', '/vendor/bin/envoy run deploy'];
+    	$process = new Process('cd .. && vendor/bin/envoy run deploy');
     	//dd('cacca');
     	//$process->start();
-    	$process->run();
-    	$process->setWorkingDirectory($command);
-    	echo $process->getOutput();
-    	echo "gagagag";
-    	echo $process->getErrorOutput();
+        //$open->run();
+        $process->run();
+        
+        echo $process->getOutput();
+        echo "gagagag";
+        echo $process->getErrorOutput();
+    	
+    	//$process->setWorkingDirectory($command);
+    	
         $err = $process->getErrorOutput();
         $warn = $process->getOutput();
         //dd($process->getOutput());
