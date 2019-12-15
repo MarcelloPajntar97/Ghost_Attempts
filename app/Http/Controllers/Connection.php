b501fba8d28518cd22cc50b57e8e447aafd90456
Guy97 <marcello.pajntar@ied.edu>
2019-12-15 22:14:08 +0100

    ssh2 connection prova

--- app/Http/Controllers/Connection.php
+++ app/Http/Controllers/Connection.php
@@ -11,29 +11,48 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Connection extends Controller
 {
     public function sshconn() {
-    	$command = base_path().'/vendor/bin/envoy run deploy';
-    	$directory = base_path();
+    	//$command = base_path().'/vendor/bin/envoy run deploy';
+    	//$directory = base_path();
         //$open = new Process('cd ..');
         //$content = ['cd ..', '/vendor/bin/envoy run deploy'];
     	//$process = new Process('cd .. && vendor/bin/envoy run deploy');
-        $process = new Process('ls');
+        //$process = new Process('ls');
     	//dd('cacca');
     	//$process->start();
         //$open->run();
-        $process->run();
-        
-        echo $process->getOutput();
-        echo "gagagag";
-        echo $process->getErrorOutput();
+        //$process->run();
+        $url = "http://159.65.168.219/conn.php";
+        $contents = file_get_contents($url);
+        $dec = json_decode($contents);
+        $convert = $dec->{'key'};
+        $convpriv = $dec->{'keypriv'};
+        //$filepub = "id_rsa.pub";
+        //Storage::disk('local')->putFile('.ssh', $filepub);
+
+
+        //echo $contents;
+        //echo $process->getOutput();
+        //echo "gagagag";
+        //echo $process->getErrorOutput();
     	
     	//$process->setWorkingDirectory($command);
     	
-        $err = $process->getErrorOutput();
-        $warn = $process->getOutput();
+        //$err = $process->getErrorOutput();
+        //$warn = $process->getOutput();
         //dd($process->getOutput());
         
-    	return view('good', compact('err', 'warn'));
+    	return view('good');
     }
 
-    
+    function connect() {
+        $connection = ssh2_connect('shell.example.com', 22, array('hostkey'=>'ssh-rsa'));
+
+if (ssh2_auth_pubkey_file($connection, 'username',
+                          '/home/username/.ssh/id_rsa.pub',
+                          '/home/username/.ssh/id_rsa', 'secret')) {
+  echo "Public Key Authentication Successful\n";
+} else {
+  die('Public Key Authentication Failed');
+}
+    }
 }
--- resources/views/good.blade.php
+++ resources/views/good.blade.php
@@ -1,9 +1,1 @@
-@if (strpos($warn, 'not found'))
-	<h2>Opss.... Qualcosa deve essere andato storto</h2><br>
-	<span>{{ $warn }}</span>
-@elseif ($err != "")
-	<h2>Opss.... Qualcosa deve essere andato storto</h2><br>
-	<span>{{ $err }}</span>
-@else
-	<h2>CONNESSOOOOO</h2>
-@endif
+schermata vuota
