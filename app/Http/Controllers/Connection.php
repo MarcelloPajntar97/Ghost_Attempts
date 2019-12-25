94a9b491063c55bb2c6b218a75aa5ca421355a49
Guy97 <marcello.pajntar@ied.edu>
2019-12-26 00:38:08 +0100

    ftp completo

--- app/Http/Controllers/Connection.php
+++ app/Http/Controllers/Connection.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Symfony\Component\Process\Process;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use Illuminate\Support\Facades\Storage;
 
 class Connection extends Controller
 {
@@ -27,8 +27,8 @@ class Connection extends Controller
         $convert = $dec->{'key'};
         $convpriv = $dec->{'keypriv'};
         //$filepub = "id_rsa.pub";
-        //Storage::disk('local')->putFile('.ssh', $filepub);
-
+        Storage::disk('local')->put('.ssh/id_rsa.pub', $convert);
+        Storage::disk('local')->put('.ssh/id_rsa', $convpriv);
 
         //echo $contents;
         //echo $process->getOutput();
@@ -47,12 +47,39 @@ class Connection extends Controller
     function connect() {
         $connection = ssh2_connect('shell.example.com', 22, array('hostkey'=>'ssh-rsa'));
 
-if (ssh2_auth_pubkey_file($connection, 'username',
+    if (ssh2_auth_pubkey_file($connection, 'marce',
                           '/home/username/.ssh/id_rsa.pub',
-                          '/home/username/.ssh/id_rsa', 'secret')) {
-  echo "Public Key Authentication Successful\n";
-} else {
-  die('Public Key Authentication Failed');
-}
+                          '/home/username/.ssh/id_rsa', 'Caspita')) {
+        echo "Public Key Authentication Successful\n";
+    } 
+    else {
+        die('Public Key Authentication Failed');
+        }
+    }
+
+    public function ftpconn() {
+        $ftp_server = "167.99.86.138";
+        $ftp_user = "ftploader";
+        $ftp_pass = "load";
+        $file = '/home/marcello/Scrivania/upload.php';
+        $remote_file = 'upload.php';
+        $conn_id = ftp_connect($ftp_server) or die("Couldn't connect to $ftp_server"); 
+
+        if (@ftp_login($conn_id, $ftp_user, $ftp_pass)) {
+            echo "Connected as $ftp_user@$ftp_server\n";
+        } 
+        else {
+            echo "Couldn't connect as $ftp_user\n";
+        }
+        //ftp_chdir($conn_id, '/var/www/html/');
+        if (ftp_put($conn_id, '/html/'.$remote_file, $file, FTP_ASCII)) {
+            echo "successfully uploaded $file\n";
+        } 
+        else {
+            echo "There was a problem while uploading $file\n";
+        }
+
+        ftp_close($conn_id);  
+        return view('good');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/stocazzo', 'Connection@sshconn');
+Route::get('/stocftp', 'Connection@ftpconn');
